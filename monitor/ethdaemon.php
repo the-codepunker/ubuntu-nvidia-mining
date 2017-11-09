f754d3709d94dcaf5bbfe8b8841217674c3ca494
the-codepunker <info@codepunker.com>
2017-11-09 22:17:35 +0200

    Added a screen session monitor and notifier
    monitor the miner screen session
    send a notification to a webhook when speed is lower than 200 or miner hangs

--- monitor/ethdaemon.php
+++ monitor/ethdaemon.php
@@ -1,6 +1,7 @@
 <?php
 
 	(php_sapi_name() === 'cli') or die("not allowed");
+	define("MONITOR_BY", "SCREEN"); // valid values are POOL and SCREEN
 
     function colorset($str, $color)
     {
@@ -21,23 +22,75 @@
         return $ansi_str;
     }
 
-    while (1) {
-		$content = file_get_contents('https://eth.nanopool.org/api/v1/workers/0x83347b2d521c697bfb6b25b3633304c1fbd9a553');
-		$content = json_decode($content);
-		$entry = null;
-		foreach ($content->data as $entry) {
-			$time_since_last_share = time() - $entry->lastShare;
-			$color = ($entry->hashrate > 40) ? 'green+bold' : 'red+bold';
-			echo colorset("$entry->id: ", $color) . round($time_since_last_share/60, 2) . "m since last share. Hashrate: " . colorset($entry->hashrate, $color) . "\n";
-		}
-		echo "Sleeping ... ";
-        for ($i=10; $i > 0; --$i) { 
-            echo $i . " ";
-            sleep(1);
-        }
-        echo "\n";
-    }
+    if(MONITOR_BY === "POOL") // connect to the pool and see how things are going
+	    while (1) {
+			$content = file_get_contents('https://eth.nanopool.org/api/v1/workers/0x83347b2d521c697bfb6b25b3633304c1fbd9a553');
+			$content = json_decode($content);
+			$entry = null;
+			foreach ($content->data as $entry) {
+				$time_since_last_share = time() - $entry->lastShare;
+				$color = ($entry->hashrate > 40) ? 'green+bold' : 'red+bold';
+				echo colorset("$entry->id: ", $color) . round($time_since_last_share/60, 2) . "m since last share. Hashrate: " . colorset($entry->hashrate, $color) . "\n";
+			}
+			echo "Sleeping ... ";
+	        for ($i=10; $i > 0; --$i) { 
+	            echo $i . " ";
+	            sleep(1);
+	        }
+	        echo "\n";
+	    }
 
-	
+	if(MONITOR_BY === "SCREEN") // connect to a screen session output and parse it
+    	$logfile = __DIR__ . '/scr.log';
+    	exec("screen -S miner -X logfile {$logfile}");
+
+    	$last = [];
+	    while (1) {
+			$content = [];
+			exec("screen -S miner -X log on");
+
+			//default screen dump to disk time is 10 secs ... so let's wait 15
+			echo "Sleeping for 15 seconds to gather some output ... \n";
+			sleep(15);
+
+			$content = file($logfile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+			$entry = null;
+
+	        exec('screen -S miner -X log off'); //disable logging
+			if(file_exists($logfile)) // remove the file
+				unlink($logfile);
+
+			if($content === $last) {
+				echo colorset("Miner hanged.", "red+bold") . "\n";
+				//trigger a webhook
+				file_get_contents("https://www.codepunker.com/tools/web-hooks/82?execute=storeWebHook&pass=8nqjmb8d&miner=hanged");
+				die;
+			}
+			$speed_found = 0;
+			foreach ($content as $entry) {
+				$matches = [];
+				if(preg_match("/Speed.{8}(\d*)\./", $entry, $matches) === 1) {
+					$speed_found = $matches[1];
+					break;
+				}
+			}
+			$color = ($speed_found > 200) ? 'green+bold' : 'red+bold';
+			echo colorset("Speed found: ", $color) . colorset($speed_found, $color) . "\n";
+
+			if($speed_found < 200) {
+				//trigger a webhook
+				file_get_contents("https://www.codepunker.com/tools/web-hooks/82?execute=storeWebHook&pass=8nqjmb8d&miner=slow");
+				die;
+			}
+
+			echo "Sleeping ... ";
+	        for ($i=10; $i > 0; --$i) { 
+	            echo $i . " ";
+	            sleep(1);
+	        }
+	        echo "\n";
+
+	        $last = $content;
+	    }
 
 	
