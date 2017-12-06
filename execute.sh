2cea1d68467f26dbe317a0937e03a0e4c68707a7
the-codepunker <info@codepunker.com>
2017-12-06 08:59:26 +0200

    changed eth address to send mining funds to

--- execute.sh
+++ execute.sh
@@ -1,1 +1,1 @@
-./ethminer --farm-recheck 1000 -U -S eth-eu1.nanopool.org:9999 -FS eth-eu2.nanopool.org:9999 -O 0x83347b2d521c697bfb6b25b3633304c1fbd9a553.danijelu1/danijelu@gmail.com -SP 0 --cuda-parallel-hash 4
+./ethminer --farm-recheck 1000 -U -S eth-eu1.nanopool.org:9999 -FS eth-eu2.nanopool.org:9999 -O 0x9335fE2BCdca68407ed5Ae5FB196d2c69CAf96Da.danijelu1/danijelu@gmail.com -SP 0 --cuda-parallel-hash 4
