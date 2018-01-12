55ff5a0cee69b99100743ce8c5bbc27dde255685
the-codepunker <info@codepunker.com>
2018-01-12 14:01:44 +0200

    added reported hashrate on stratum

--- execute.sh
+++ execute.sh
@@ -1,1 +1,1 @@
-./ethminer --farm-recheck 1000 -U -S eth-eu1.nanopool.org:9999 -FS eth-eu2.nanopool.org:9999 -O 0x9335fE2BCdca68407ed5Ae5FB196d2c69CAf96Da.danijelu1/danijelu@gmail.com -SP 1 --cuda-parallel-hash 4
+./ethminer --farm-recheck 1000 -U -S eth-eu1.nanopool.org:9999 -FS eth-eu2.nanopool.org:9999 -O 0x9335fE2BCdca68407ed5Ae5FB196d2c69CAf96Da.danijelu1/danijelu@gmail.com -RH -SP 1 --cuda-parallel-hash 4
